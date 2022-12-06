77c296beace279d45bd83b434d17286f9cbb0765
Carla <109388578+CarlaBenavidez@users.noreply.github.com>
2022-12-06 13:25:10 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+/*!
+* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+//
+// Scripts
+//
+
+window.addEventListener("DOMContentLoaded", event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector("#mainNav");
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove("navbar-shrink")
+        } else {
+            navbarCollapsible.classList.add("navbar-shrink")
+        }
+
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener("scroll", navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector("#mainNav");
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: "#mainNav",
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll("#navbarResponsive .nav-link")
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener("click", () => {
+            if (window.getComputedStyle(navbarToggler).display !== "none") {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: "#portfolio a.portfolio-box"
+    });
+
+});
+
+// Flecha que lleva arriba de la pagina
+
+$(document).ready(function(){
+
+	$(".iconowap").click(function(){
+		$("body, html").animate({
+			scrollTop: "0px"
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$(".iconowap").slideDown(300);
+		} else {
+			$(".iconowap").slideUp(300);
+		}
+	});
+
+});
